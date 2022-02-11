7df70b98af91e7d24f36c3e74b2fa0c2479a2638
TedCadet <e_cadet22@hotmail.com>
2022-02-11 18:41:40 -0500

    rpc readBlog added

--- src/main/java/com/tedcadet/grpc/blog/server/BlogServiceImpl.java
+++ src/main/java/com/tedcadet/grpc/blog/server/BlogServiceImpl.java
@@ -4,14 +4,15 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import com.proto.blog.Blog;
-import com.proto.blog.BlogServiceGrpc;
-import com.proto.blog.CreateBlogRequest;
-import com.proto.blog.CreateBlogResponse;
+import com.proto.blog.*;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static com.mongodb.client.model.Filters.eq;
 
 
 public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
@@ -25,7 +26,7 @@ public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
     private MongoCollection<Document> collection = database.getCollection("blog");
 
     @Override
-    public void createBlog(CreateBlogRequest request, StreamObserver<CreateBlogResponse> responseObserver) {
+    public void createBlog(BlogRequest request, StreamObserver<BlogResponse> responseObserver) {
 
         // login to db
 
@@ -50,7 +51,7 @@ public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
 
         // return the blog created in mongodb
         logger.info("sending response");
-        responseObserver.onNext(CreateBlogResponse.newBuilder()
+        responseObserver.onNext(BlogResponse.newBuilder()
                 .setBlog(blog.toBuilder()
                         .setId(id)
                         .build())
@@ -59,5 +60,44 @@ public class BlogServiceImpl extends BlogServiceGrpc.BlogServiceImplBase {
         // close the call
         logger.info("request completed");
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void readBlog(ReadBlogRequest request, StreamObserver<BlogResponse> responseObserver) {
+
+        // get the request
+        logger.info("Received a create Blog request");
+        String blogId = request.getBlogId();
+
+        // find the blog in the db
+        logger.info("Searching in db for the blog...");
+        Document searchResult = collection.find(eq("_id", new ObjectId(blogId)))
+                .first();
+
+        if(searchResult == null) {
+            responseObserver.onError(
+                    Status.NOT_FOUND
+                            .withDescription("The blog with the corresponding id was not found")
+                            .asRuntimeException());
+        } else {
+
+            Blog blogFounded = Blog.newBuilder()
+                    .setId(searchResult.getObjectId("_id").toString())
+                    .setAuthorId(searchResult.getString("author_id"))
+                    .setTitle(searchResult.getString("title"))
+                    .setContent(searchResult.getString("title"))
+                    .build();
+
+            // create a response
+            BlogResponse response = BlogResponse.newBuilder()
+                    .setBlog(blogFounded)
+                    .build();
+
+            // return the response
+            responseObserver.onNext(response);
+
+            // close the call
+            responseObserver.onCompleted();
+        }
     }
 }
